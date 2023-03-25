382b63c0f3708c54c1364d91c279f0a133e48b29
Justin Tran <justin.tran@haw-hamburg.de>
2023-03-25 11:14:22 +0100

    added custom json structure

--- issue_parser.py
+++ issue_parser.py
@@ -1,7 +1,11 @@
 from github import Github
+import array
+import json
+from tqdm import tqdm
+
 
 class GithubIssueParser:
-    def __init__(self, access_token : str, repo : str = "RIOT-OS/RIOT"):
+    def __init__(self, access_token: str, ignored_users: array, repo: str = "RIOT-OS/RIOT"):
         """
         Initializes an instance of a Github Issue Parser
 
@@ -11,23 +15,27 @@ class GithubIssueParser:
             Github access token, must be created manually
         repo : str
             Github repository that should be accessed, syntax: "<owner>/<repo>"
+        ignored_users : array
+            Array contains users to be ignored (e.g. bots)
         """
+        self._ignored_users = ignored_users
         print(f'Initializing Github Issue Parser for repository {repo}')
         self._g = Github(access_token)
         self._repo = self._g.get_repo(repo)
 
     def _get_issues_and_remove_prs(self):
         issues = self._repo.get_issues(state='open')
-        issues = [i for i in issues if i.pull_request == None]
+        print('Looking for open issues...')
+        issues = [issue for issue in tqdm(issues) if issue.pull_request is None]
+        print(f'Found {len(issues)} open issues')
         return issues
 
     def _count_issue_engagement(self, issues):
-        ignored_users = ["riot-ci"]
         issue_engagement = 0
         if issues.comments > 0:
             comments = issues.get_comments()
             for comment in comments:
-                if comment.user.login not in ignored_users:
+                if comment.user.login not in self._ignored_users:
                     issue_engagement += 1
         return issue_engagement
 
@@ -50,6 +58,13 @@ class GithubIssueParser:
                     issue_type = types
         return area, issue_type
 
+    def _issues_to_json(self, issue_list):
+        issue_dict = {"tickets": []}
+        for issue in issue_list:
+            issue_dict["tickets"].append(issue)
+        issue_json = json.dumps(issue_dict, indent=4)
+        return issue_json
+
     def get_issues(self):
         """
         Returns a list of Github issues.
@@ -67,7 +82,8 @@ class GithubIssueParser:
         issue_list = []
         issues = self._get_issues_and_remove_prs()
 
-        for issue in issues:
+        print("Converting issues into custom format...")
+        for issue in tqdm(issues):
             tmp = {
                 "ticket": {
                     "ticket-id": 0,
@@ -86,4 +102,5 @@ class GithubIssueParser:
 
             tmp["ticket"]["date"] = issue.created_at.strftime("%x").replace("/", "-")
             issue_list.append(tmp)
-        return issue_list
+        issue_json = self._issues_to_json(issue_list)
+        return issue_json
